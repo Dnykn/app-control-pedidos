b88613ed931f8d21c82bf54179a92871e6e927c2
Andy <andycampos971@gmail.com>
2025-11-10 10:35:55 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBc_I890BGvQD4_b-1Djx0vDkxLH6hiys0",
-    authDomain: "app-pedidos-a73bf.firebaseapp.com",
-    projectId: "app-pedidos-a73bf",
-    storageBucket: "app-pedidos-a73bf.firebasestorage.app",
-    messagingSenderId: "615368498959",
-    appId: "1:615368498959:web:8183329ede0dbbf6f3687a"
+    apiKey: your api key
 };
 
 export const app = initializeApp(firebaseConfig);
